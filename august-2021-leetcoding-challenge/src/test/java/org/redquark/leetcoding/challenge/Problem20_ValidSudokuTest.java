dbf4c7081f4f1bd0a5749614bbe8fe94c3b0621a
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-08-20 14:44:19 +0530

    Problem 20 - Valid Sudoku

---
+++ august-2021-leetcoding-challenge/src/test/java/org/redquark/leetcoding/challenge/Problem20_ValidSudokuTest.java
@@ -0,0 +1,40 @@
+package org.redquark.leetcoding.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Problem20_ValidSudokuTest {
+
+    private final Problem20_ValidSudoku testObject = new Problem20_ValidSudoku();
+
+    @Test
+    void testIsValidSudoku() {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'}
+                , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        assertTrue(testObject.isValidSudoku(board));
+
+        board = new char[][]{
+                {'8', '3', '.', '.', '7', '.', '.', '.', '.'}
+                , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        assertFalse(testObject.isValidSudoku(board));
+    }
+}
